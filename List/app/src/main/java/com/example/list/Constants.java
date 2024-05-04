6dbaa0fceb50fba0fddbb237c19b3669bb691531
Jehad Cemanes <jehadcemanesece@gmail.com>
2024-05-04 17:20:45 +0800

    Add List View

---
+++ List/app/src/main/java/com/example/list/Constants.java
@@ -0,0 +1,16 @@
+package com.example.list;
+
+public class Constants {
+    public static final String[] products = {
+            "Product 1",
+            "Product 2",
+            "Product 3",
+            "Product 4",
+            "Product 5",
+            "Product 6",
+            "Product 7",
+            "Product 8",
+            "Product 9",
+            "Product 10"
+    };
+}
---
+++ List/app/src/main/java/com/example/list/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.list;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.item_product, Constants.products);
+        ListView listView = findViewById(R.id.list);
+        listView.setAdapter(adapter);
+    }
+}
